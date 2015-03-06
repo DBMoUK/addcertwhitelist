9bea02b5422c1acb07ebe712254e35ce5da24cb8
David Bryant-Moore <david.bmoore@puppetlabs.com>
2015-03-06 22:31:50 +0000

    Refacter parent class

---
+++ manifests/init.pp
@@ -0,0 +1,4 @@
+class certwhitelist {
+  $templatepath             = '/opt/puppet/share/puppet/modules/puppet_enterprise/templates/master/puppetserver'
+  $console_client_certname  = 'pe-internal-dashboard'
+}
---
+++ tests/restart.pp
@@ -0,0 +1,2 @@
+include certwhitelist::restart
+
